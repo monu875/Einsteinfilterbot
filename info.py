e7723dddd73957e65d2c0ad7ea2857dc1c605b00
leomemlon <139001721+monu875@users.noreply.github.com>
2023-09-08 00:54:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -94,9 +94,9 @@ MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 
-DELETE_TIME = int(environ.get('DELETE_TIME', 300))
-CH_LINK = environ.get('CH_LINK', "https://t.me/+eqYcI8xNKzs2MzEx")
-CH_FILTER = int(environ.get('CH_FILTER', -1001546043562))
+DELETE_TIME = int(environ.get('DELETE_TIME', 800))
+CH_LINK = environ.get('CH_LINK', "https://t.me/+44kDR8AMkCBlM2Y1")
+CH_FILTER = int(environ.get('CH_FILTER', -1001637697616))
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
